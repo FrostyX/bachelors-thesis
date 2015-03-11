392ee440254a5a145b9fd9e9e8a334107cc0824b
Jakub Kadlčík <frostyx@email.cz>
2015-03-11 18:47:56 +0100

    Opraveno odsazení pseudokódu

--- sources/tracer_algorithm.py
+++ sources/tracer_algorithm.py
@@ -2,6 +2,6 @@ packages = seznam balíčků modifikovaných od spuštění systému
 processes_files = BTree(...)
 for package in packages:
     for package_file in package_files:
-	processes = processes_files[package_file]
-	if processes != prázdná množina:
-	    print processes
+        processes = processes_files[package_file]
+        if processes != prázdná množina:
+            print processes
