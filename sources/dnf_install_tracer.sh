c6f1ec15c2bee75d563dbe14b296c84f1ba21cc9
Jakub Kadlčík <frostyx@email.cz>
2015-04-14 13:56:26 +0200

    Napsána kapitola Balíčey pro Fedoru

---
+++ sources/dnf_install_tracer.sh
@@ -0,0 +1,6 @@
+# Instalace pouze programu Tracer
+sudo dnf install tracer
+
+# Instalace včetně DNF pluginu
+# Netřeba explicitně instalovat balík 'tracer'
+sudo dnf install dnf-plugins-extras-tracer
---
+++ sources/rpm_install_tracer.sh
@@ -0,0 +1,8 @@
+# @TODO Dopsat příkaz po zjištění struktury přikládaného CD
+
+# Instalace pouze programu Tracer
+sudo rpm -i /cesta/k/balicku/tracer.rpm
+
+# Instalace včetně DNF pluginu
+# Netřeba explicitně instalovat balík 'tracer'
+sudo rpm -i /cesta/k/balicku/dnf-plugins-extras-tracer.rpm
