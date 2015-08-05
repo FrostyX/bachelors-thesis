b6a9000e22a19a5b22570c6e51201f17480f9af1
Jakub Kadlčík <frostyx@email.cz>
2015-08-05 08:11:31 +0200

    Instalace přes DNF je lepší

--- sources/rpm_install_tracer.sh
+++ sources/rpm_install_tracer.sh
@@ -2,8 +2,8 @@
 cd /run/media/<user>/<drive>/install/
 
 # Instalace pouze programu Tracer
-sudo rpm -i ./tracer-0.6.2-1.fc22.noarch
+sudo dnf install ./tracer-0.6.2-1.fc22.noarch
 
 # Instalace včetně DNF pluginu
 # Netřeba explicitně instalovat balík 'tracer'
-sudo rpm -i ./python-dnf-plugins-extras-tracer-0.0.9-1.fc22.noarch
+sudo dnf install ./python-dnf-plugins-extras-tracer-0.0.9-1.fc22.noarch
